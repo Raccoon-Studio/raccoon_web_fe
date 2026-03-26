0b90fd48829b66c888942aee397165c2526a1e58
Ankit Dalei <134222760+Ankit-Dalei@users.noreply.github.com>
2026-03-27 00:49:07 +0530

    feat: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+import unusedImportsPlugin from "eslint-plugin-unused-imports";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      "unused-imports": unusedImportsPlugin,
+    },
+    rules: {
+      "max-lines": [
+        "error",
+        { max: 280, skipBlankLines: true, skipComments: true },
+      ],
+      "prettier/prettier": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "no-unreachable": "error",
+      "no-empty": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
+  prettierConfig,
+]);
+
+export default eslintConfig;
